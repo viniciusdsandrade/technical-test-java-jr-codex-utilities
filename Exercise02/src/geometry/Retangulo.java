8e029ade4d59e1b973dbdeee981d0f7da93054c3
Vinicius <vinicius_andrade2010@hotmail.com>
2024-10-04 11:29:16 -0300

    Remove .env from version control

--- Exercise02/src/geometry/Retangulo.java
+++ Exercise02/src/geometry/Retangulo.java
@@ -85,15 +85,12 @@ public class Retangulo implements Cloneable {
     public int getX1() {
         return x1;
     }
-
     public int getY1() {
         return y1;
     }
-
     public int getX2() {
         return x2;
     }
-
     public int getY2() {
         return y2;
     }
@@ -110,17 +107,14 @@ public class Retangulo implements Cloneable {
         this.x1 = x1;
         validateRectangle();  // Valida após a modificação
     }
-
     public void setY1(int y1) {
         this.y1 = y1;
         validateRectangle();  // Valida após a modificação
     }
-
     public void setX2(int x2) {
         this.x2 = x2;
         validateRectangle();  // Valida após a modificação
     }
-
     public void setY2(int y2) {
         this.y2 = y2;
         validateRectangle();  // Valida após a modificação
